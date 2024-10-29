4577aac952af3860f5020ee3b0b2382d6fb00d59
Olle <simonsenolle@gmail.com>
2024-10-29 16:48:52 +0100

    Small change in Strengthworkout.cs

--- FitTrackApp/Models/StrengthWorkout.cs
+++ FitTrackApp/Models/StrengthWorkout.cs
@@ -12,7 +12,8 @@ namespace FitTrackApp.Models
 
         public override int CalculateCaloriesBurned() // Another override in order to calculate the calories burned. 
         {
-            CaloriesBurned = Repetitions; // * 2? Or what should the formula be here, ask tmr, can only find way more specific formulas online. 
+            CaloriesBurned = Repetitions * 2;
+            return CaloriesBurned; // What should the formula be here, ask tmr, can only find way more specific formulas online. 
         }
     }
 }
