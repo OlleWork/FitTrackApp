2cd731a3fc9989a989e63264b83fd84d008748ab
Olle <simonsenolle@gmail.com>
2024-10-29 16:16:30 +0100

    Added all folders, cs, and xaml files as well as starting with classes one by one following the template provided.

---
+++ FitTrackApp/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrackApp.Models
+{
+    public abstract class Person
+    {
+        // Public way to store all credentials. 
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+
+
+        // Method for signing in. If true it returns successfully.
+        public abstract bool SignIn(string username, string password);
+    }
+}
+
---
+++ FitTrackApp/Models/Workout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrackApp.Models
+{
+    public abstract class Workout
+    {
+        
+    }
+}
