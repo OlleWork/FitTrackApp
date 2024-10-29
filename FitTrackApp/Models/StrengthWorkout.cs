68be1d0755e42aa38c3c86e5c22e8c7c7fbc91d0
Olle <simonsenolle@gmail.com>
2024-10-29 16:35:50 +0100

    Created StrengthWorkout, have not added a formula yet.

--- FitTrackApp/Models/StrengthWorkout.cs
+++ FitTrackApp/Models/StrengthWorkout.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace FitTrackApp.Models
 {
-    class StrengthWorkout
+    class StrengthWorkout : Workout
     {
+        public int Repetitions { get; set; } // In order for the repitions to count.
+
+        public override int CalculateCaloriesBurned() // Another override in order to calculate the calories burned. 
+        {
+            CaloriesBurned = Repetitions; // * 2? Or what should the formula be here, ask tmr, can only find way more specific formulas online. 
+        }
     }
 }
