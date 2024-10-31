1d144a7ecbd211b70766b831ee335a769d5c7aed
Olle <simonsenolle@gmail.com>
2024-10-31 20:16:29 +0100

    Added User button in works. Added comments.

--- FitTrackApp/ViewModels/WorkoutsViewModel.cs
+++ FitTrackApp/ViewModels/WorkoutsViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using FitTrack.PropChange;
 using FitTrackApp.Models;
+using FitTrackApp.Views;
 using FitTrackApp.VMB_RC;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Input;
 
 namespace FitTrackApp.ViewModels
 {
-    internal class WorkoutsViewModel : ViewModelBase, INotifyPropertyChanged
+    public class WorkoutsViewModel : INotifyPropertyChanged
     {
         // ObservableCollection to hold and update the user list for use in the View
         public ObservableCollection<User> Users { get; set; }
@@ -15,9 +17,9 @@ namespace FitTrackApp.ViewModels
 
         public string DisplayUser => UserService.Instance.CurrentUser?.Username ?? "Guest Mode"; // This shows the logged in users username if not logged in, Guest Mode is displayed.
 
+        public ICommand UserInfo { get; } // Binding the button with the viewmodel
 
-
-
+        
         public WorkoutsViewModel() // Default Constructor
         {
             // Ingen extra kod behövs för att hämta Workouts eftersom Workouts kan nu bindas direkt
@@ -26,8 +28,22 @@ namespace FitTrackApp.ViewModels
         public WorkoutsViewModel(List<User> users) // A Constructor to initialize Users collection.
         {
             Users = new ObservableCollection<User>(users);
+
+            UserInfo = new RelayCommand(_ => UserInfoDetails());
         }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        private void UserInfoDetails()
+        {
+            UserDetailsWindow userDetailsWindow = new UserDetailsWindow();
+
+            userDetailsWindow.ShowDialog(); // Shows UserDetailsWindow as a dialog.
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged; // Databinding support. 
+        protected void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
